4291088457b86e678d214e06c0e90624d6bb0ed0
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-03-14 15:19:45 -0700

    Postgres and proftpd configuration
     - galaxy uses postgres, not mysql 8-\
     - configure proftpd with template from this cookbook
       - the proftp_vhost resource doesn't seem to enable us
         to provide our own variables

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+
+node.default['galaxy_ftp']['db_uri'] = 'database@host:port'
+node.default['galaxy_ftp']['db_user'] = 'username'
+node.default['galaxy_ftp']['db_user_pw'] = 'a_secret'
+
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,4 +10,24 @@ include_recipe 'proftpd-ii'
 # Add postgres support to proftpd
 package 'proftpd-mod-pgsql'
 proftpd_module 'pgsql'
+#
+# Add vhost configuration from template
 
+template "#{node['proftpd-ii']['conf_dir']}/sites-available/galaxy-ftp.conf" do
+  owner node['proftpd-ii']['user']
+  group node['proftpd-ii']['group']
+  mode 0640
+  variables(
+    'db_uri' => node['galaxy_ftp']['db_uri'],
+    'db_user' => node['galaxy_ftp']['db_user'],
+    'db_user_pw' => node['galaxy_ftp']['db_user_pw']
+  )
+  source 'vhost/galaxy-ftp.erb'
+  notifies :create, "link[#{node['proftpd-ii']['conf_dir']}/sites-available/galaxy-ftp.conf]", :immediate
+end
+
+link "#{node['proftpd-ii']['conf_dir']}/sites-available/galaxy-ftp.conf" do
+  to "#{node['proftpd-ii']['conf_dir']}/sites-enabled/galaxy-ftp.conf" 
+  link_type :symbolic
+  action :nothing
+end
