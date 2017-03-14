3db7e9a38b53e5a79d7eb735d9cc97167249e261
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-03-14 16:22:03 -0700

    Use vault for recipe data
    Updated to look for database connection parameters in chef-vault

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,3 @@
 
-node.default['galaxy_ftp']['db_uri'] = 'database@host:port'
-node.default['galaxy_ftp']['db_user'] = 'username'
-node.default['galaxy_ftp']['db_user_pw'] = 'a_secret'
+node.default['galaxy_ftp']['vault'] = 'vault'
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,11 @@
 # Copyright (c) 2017 The Authors, All Rights Reserved.
 #
 
+include_recipe 'chef-vault::default'
+db_uri = chef_vault_item(node['galaxy_ftp']['vault'], 'db')['uri']
+db_user = chef_vault_item(node['galaxy_ftp']['vault'], 'db')['user']
+db_user_pw = chef_vault_item(node['galaxy_ftp']['vault'], 'db')['user_pw']
+
 include_recipe 'proftpd-ii'
 
 # Add postgres support to proftpd
@@ -18,9 +23,9 @@ template "#{node['proftpd-ii']['conf_dir']}/sites-available/galaxy-ftp.conf" do
   group node['proftpd-ii']['group']
   mode 0640
   variables(
-    'db_uri' => node['galaxy_ftp']['db_uri'],
-    'db_user' => node['galaxy_ftp']['db_user'],
-    'db_user_pw' => node['galaxy_ftp']['db_user_pw']
+    'db_uri' => db_uri,
+    'db_user' => db_user,
+    'db_user_pw' => db_user_pw
   )
   source 'vhost/galaxy-ftp.erb'
   notifies :create, "link[#{node['proftpd-ii']['conf_dir']}/sites-enabled/galaxy-ftp.conf]", :immediate
