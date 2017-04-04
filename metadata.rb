a56c90227771eca71f688eec8958a4f63ffedee0
Michael Gutteridge <michael.gutteridge@gmail.com>
2017-04-04 13:57:11 -0700

    1.0.0 Release

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mrg@fredhutch.org'
 license 'mit'
 description 'Install an FTP server for uploading data into a galaxy server'
 long_description ''
-version '0.2.2'
+version '1.0.0'
 
 issues_url 'https://github.com/../galaxy_ftp/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/../galaxy_ftp' if respond_to?(:source_url)
