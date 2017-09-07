53ee36a82f3b8224d9a2de0753f1d9433351c89c
Suchismita panda <suchismita.panda@outlook.com>
2017-09-07 14:58:36 -0700

    changing the metadata file for foodcritic

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,8 @@ license 'mit'
 description 'Install an FTP server for uploading data into a galaxy server'
 long_description ''
 version '0.2.2'
-
+chef_version '>= 12.1' if respond_to?(:chef_version)
+supports 'ubuntu'
 issues_url 'https://github.com/../galaxy_ftp/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/../galaxy_ftp' if respond_to?(:source_url)
 
